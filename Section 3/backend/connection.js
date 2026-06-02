deb08f2d875bee2d63c812dd594a1635a913320f
shabdsri20 <shabd200605@gmail.com>
2026-06-02 17:15:41 +0530

    ..

--- Section 3/backend/connection.js
+++ Section 3/backend/connection.js
@@ -5,6 +5,7 @@ dns.setServers(["1.1.1.1", "8.8.8.8"]);
 const url = "mongodb+srv://shabd:setup123@cluster0.kuaiyhp.mongodb.net/mydb?appName=Cluster0"
 
 mongoose.connect(url)
+
 .then((result) => {
     console.log("DataBase Connected");
 })
--- Section 3/backend/routers/userRouter.js
+++ Section 3/backend/routers/userRouter.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get( '/add', (req,res) =>{
+router.post( '/add', (req,res) =>{
     res.send('response from user add');
 
 });
+
+router.get( '/getall', (req,res) =>{
+    res.send('response from user getAll');
+
+});
+
+router.put( '/update', (req,res) =>{
+    res.send('response from user update');
+
+});
+
+router.delete( '/delete', (req,res) =>{
+    res.send('response from user delete');
+
+});
+
 
 module.exports = router;
